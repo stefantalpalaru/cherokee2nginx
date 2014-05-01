4263c065b364008cd06d3ab115f7bbbdbe21f22a
Stefan Talpalaru <stefantalpalaru@yahoo.com>
2014-05-01 18:40:45 +0200

    add copyright info

--- cherokee2nginx.py
+++ cherokee2nginx.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+
+# Copyright © 2014 - Stefan Talpalaru <stefantalpalaru@yahoo.com>
 
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
